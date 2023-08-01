fa2b09184469ce6bb67d7359709d5797d5479f72
SkadiDonovan <fant1maz@yandex.ru>
2023-08-01 17:51:05 +0300

    chahges HelloController

--- src/main/java/ru/skadidonovan/prodforjenkins/config/controllers/HelloController.java
+++ src/main/java/ru/skadidonovan/prodforjenkins/config/controllers/HelloController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping()
     public String hello(){
-        return "Hello, Jenkins. How are you";
+        return "Hello, Jenkins. How are you?";
     }
 }
